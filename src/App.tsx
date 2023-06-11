19b247fb6b08e00c52aa796c81ab508b093b918c
Jukka Puranen <jukka.puranen@mogrouppartners.com>
2023-06-11 11:59:58 +0300

    Maiview header and decklist search field added
    - Also github actions now should run against development branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='row'>
+          <h1>Yet another ringsdb client</h1>
+        </div>
+        <div className='row'>
+          <form>
+            <div className="mb-3">
+              <label  className="form-label">
+                Search for a decklist here
+                <input type="text" className="form-control" id="deckSearch" placeholder="Decklist id" /> 
+              </label>           
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- tests/mainView.spec.ts
+++ tests/mainView.spec.ts
@@ -1,8 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test('has page title', async ({ page }) => {
+    await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Yet another ringdb client/);
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Yet another ringsdb client/);
 });
+
+test('has view heading', async ({ page }) => { 
+    const expectedHeaderTitle = 'Yet another ringsdb client'
+    await page.goto('/');
+    const heading = page.getByRole('heading', { name: expectedHeaderTitle });
+    await expect(heading).toContainText(expectedHeaderTitle);
+
+});
+
+test('has deckId input', async ({ page }) => {
+    const expectedLabelText = 'Search for a decklist here';
+    await page.goto('/');
+   const deckIdInputLocator =  page.getByLabel(expectedLabelText);
+   await expect(deckIdInputLocator).toBeVisible();
+   
+  });
