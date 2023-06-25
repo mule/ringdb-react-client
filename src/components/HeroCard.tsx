52907d5004b1632017f068e2e90a175b90210c01
Jukka Puranen <jukka.puranen@mogrouppartners.com>
2023-06-25 22:14:41 +0300

    Added tooltip to improve UX

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -1,7 +1,8 @@
-import { Card, Col, Toast } from "react-bootstrap";
+import { Card, Col, OverlayTrigger, Toast, Tooltip, TooltipProps } from "react-bootstrap";
 import { useQuery } from "@tanstack/react-query";
 import { HeroCardDetails } from "./HeroCardDetails";
-import { useState } from "react";
+import { RefAttributes, useRef, useState } from "react";
+import { JSX } from "react/jsx-runtime";
 
 export interface HeroCardProps {
     Id: string;
@@ -9,6 +10,7 @@ export interface HeroCardProps {
 }
 
 export function HeroCard(props: HeroCardProps) {
+
 
     const [show, setShow] = useState(false);
 
@@ -42,35 +44,50 @@ export function HeroCard(props: HeroCardProps) {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-  
+    const renderTooltip = (props: JSX.IntrinsicAttributes & TooltipProps & RefAttributes<HTMLDivElement>) => (
+        <Tooltip id="tooltip" {...props}>Click card for details</Tooltip>
+    );
+
+
 
     return (
-    <Col key={props.Id}>
-        <Card style={{ width: '18rem' }} onClick={handleShow}>
-            <Card.Body>
-                {isLoading &&
-                    <Toast>
-                        <Toast.Header>
-                            <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-                            <strong className="me-auto">RingsDB client</strong>
+        <Col key={props.Id}>
 
-                        </Toast.Header>
-                        <Toast.Body>Loading hero card</Toast.Body>
-                    </Toast>}
-                {data && <>
-                    <Card.Img variant="top" src={`https://digital.ringsdb.com/${data.imagesrc}`} alt={data.name} />
-                </>}
-                {isError &&
-                    <Toast>
-                        <Toast.Header>
-                            <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-                            <strong className="me-auto">RingsDB client</strong>
+            <Card style={{ width: '18rem' }} onClick={handleShow}>
+                <OverlayTrigger
+                    placement="bottom"
+                    delay={{ show: 250, hide: 400 }}
+                    overlay={renderTooltip}
+                    
+                >
+                    <Card.Body>
+                        {isLoading &&
+                            <Toast>
+                                <Toast.Header>
+                                    <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                                    <strong className="me-auto">RingsDB client</strong>
 
-                        </Toast.Header>
-                        <Toast.Body>Failed to load data</Toast.Body>
-                    </Toast>}
-            </Card.Body>
-        </Card>
-        <HeroCardDetails {...data}  show={show} onHide={handleClose} />
-    </Col>);
+                                </Toast.Header>
+                                <Toast.Body>Loading hero card</Toast.Body>
+                            </Toast>}
+                        {data && <>
+
+                            <Card.Img variant="top" src={`https://digital.ringsdb.com/${data.imagesrc}`} alt={data.name} />
+
+                        </>}
+                        {isError &&
+                            <Toast>
+                                <Toast.Header>
+                                    <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                                    <strong className="me-auto">RingsDB client</strong>
+
+                                </Toast.Header>
+                                <Toast.Body>Failed to load data</Toast.Body>
+                            </Toast>}
+                    </Card.Body>
+                </OverlayTrigger>   
+            </Card>
+
+            <HeroCardDetails {...data} show={show} onHide={handleClose} />
+        </Col>);
 }
